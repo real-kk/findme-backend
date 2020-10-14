9849de854b8fc67aba13854802f0ceaee4349568
Kim Tae Yun <rlaxodbs2015@gmail.com>
2020-10-14 19:09:55 +0900

    make email redundance check Api
     email is exist : 403
     email isn't exist  : 200

--- findme/findme/urls.py
+++ findme/findme/urls.py
@@ -18,12 +18,11 @@ from django.urls import path
 from django.conf.urls import include, url
 from allauth.account.views import confirm_email
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
     url(r'^accounts-rest/registration/account-confirm-email/(?P<key>.+)/$', confirm_email,
         name='account_confirm_email'),
-
+    path('users/', include('users.urls')),
 ]
--- findme/users/views.py
+++ findme/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from .models import User
+from django.http import HttpResponse
+
+def EamilRedundantCheck(request) :
+    if request.method == "GET":
+        email = request.GET.get("email")
+        try:
+            userExist=len(User.objects.filter(email=email))
+        except:
+            return HttpResponse('Server Error',status=403)
+
+        
+        if userExist ==False:
+            return HttpResponse('Email Available',status=200)
+        else:
+            return HttpResponse('Email Redundant',status=403)
