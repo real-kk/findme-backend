92a28d57df151ae78cac8907e51039d4227e4dce
nuatmochoi <tjddn8770@naver.com>
2020-10-22 13:22:44 +0900

    Modify: hide secret_key and email_info

--- findme/findme/settings.py
+++ findme/findme/settings.py
@@ -11,16 +11,17 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import json
+
+from findme.keys import email
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jfilzy#e6o1u8=usyctt_!=ik3b=y-*&yl)4ik-#)pn5tl!5sn'
+# Django Project Secret Key
+with open(os.path.join(BASE_DIR, 'findme/keys/secret_key.json')) as f:
+    SECRET_KEY = json.loads(f.read())['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -68,8 +69,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'rlaxodbs2015@gmail.com'
-EMAIL_HOST_PASSWORD = 'wmf128!@#'
+
+EMAIL_HOST_USER = email.EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = email.EMAIL_HOST_PASSWORD
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
