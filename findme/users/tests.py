1dcdeb08bfe70c3cbcd82110e2cd5ce9cb68f9d3
Kim Tae Yun <rlaxodbs2015@gmail.com>
2020-10-23 21:15:43 +0900

    Add Test( Login )

--- findme/users/tests.py
+++ findme/users/tests.py
@@ -4,28 +4,41 @@ from .models import User
 class UserTest(TestCase):
     # test용 데이터
     def setUp(self):
-        User.objects.create(email='rla@gmail.com')
+        self.client = Client()
+        self.user= User()
     
+        user=User.objects.create(email="rlaa@gmail.com")
+        user.set_password('abcd123!!!')
+        user.save()
     # 이메일 중복 test
     def test_redundant_check(self):
-        client = Client() 
-        response = self.client.get('/users/email',data={'email':'rla@gmail.com'})
+        response = self.client.get('/users/email',data={'email':'rlaa@gmail.com'})
         self.assertEqual(response.status_code, 403) 
+
     # 회원가입 test
     def test_user_registration(self):
-        client = Client()
 
         regi_info ={
-            "username": "kimtaeyun",
-            "email":"rlarla@gamil.com",
-            "user_type":"1", "password1":"dktnlqek123!@#",
-            "password2":"dktnlqek123!@#" 
+            "username": "",
+            "email":"@gmail.com",
+            "user_type":"1",
+            "password1":"!@#",
+            "password2":"!@#" 
         }
-        
         response= self.client.post('/rest-auth/registration/',data=regi_info)
         self.assertEqual(response.status_code,201)
         self.assertEqual(len(response.json().get("key"))>10,True)
     #로그인 test
     def test_user_login(self):
         #smoke test
-        assert 1 is 1
+        login_info={
+            'email' : '',
+            'password' : 'abcd123!!!',
+            'user_type': 1
+        }
+        #로그인이 안됨
+        response = self.client.post('/rest-auth/login/',data=login_info)
+        print(response.json())
+        
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(len(response.json().get("key"))>10,True)
