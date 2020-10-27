dc51775727145b05f4394158c0778c48bd4f7fe7
Kim Tae Yun <rlaxodbs2015@gmail.com>
2020-10-28 01:24:27 +0900

    Modify : Task ( set more attributes / set nullable Video )
    Should make put function in View.py  ( update Task With videofile     By taskID )

--- findme/task/models.py
+++ findme/task/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.conf import settings
 
 class Task(models.Model):
-    
+    client = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="+", on_delete=models.CASCADE, null=True)
+    create_date = models.DateTimeField(auto_now_add=True, null=True)
     title= models.CharField(max_length=200)
-    video= models.FileField()
+    video= models.FileField(upload_to="video/", blank=True ,null=True)
+    class Meta:
+        verbose_name = '영상과제'
+
     def __str__(self):
         return self.title
-    
