e01c0977b06b36c3dc48f82bf76045f261ef9085
nuatmochoi <tjddn8770@naver.com>
2020-10-29 04:40:19 +0900

    Modify: datetime editable

--- findme/counsel/models.py
+++ findme/counsel/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.conf import settings
+
+
 def upload_image_to(instance, filename):
     import os
     filename_base, filename_ext = os.path.splitext(filename)
     return 'image/timetable/%s%s' % (filename_base,filename_ext )
-
-
 
 class Counsel(models.Model):
     client = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="+", on_delete=models.CASCADE, null=False)
@@ -15,7 +15,7 @@ class Counsel(models.Model):
     phone_number = models.CharField(max_length=100)
     time_table = models.ImageField(upload_to=upload_image_to, blank=True, null=True)
     create_date = models.DateTimeField(auto_now_add=True, null=True)
-    create_date.editable=True
+    # create_date.editable=True
     class Meta:
         verbose_name = '신청서'
     
