e242a29f0e6e9c2875dbcc7cbdf55f5361596f44
Kim Tae Yun <rlaxodbs2015@gmail.com>
2020-10-29 03:32:47 +0900

    Fix : Counsel post setting
    1. setting : admin page에서  counsel을 보기 좋게 만듬
    2. 응답으로 신청 정보들 잘 돌아가도록
    3.  git ignore추가

--- findme/counsel/models.py
+++ findme/counsel/models.py
@@ -2,11 +2,8 @@ from django.db import models
 from django.conf import settings
 def upload_image_to(instance, filename):
     import os
-    from django.utils.timezone import now
     filename_base, filename_ext = os.path.splitext(filename)
-    return 'image/timetable/%s/%s.%s' % (
-        now().strftime("%Y%m%d")  ,filename_base,filename_ext     
-    )
+    return 'image/timetable/%s%s' % (filename_base,filename_ext )
 
 
 
@@ -18,7 +15,10 @@ class Counsel(models.Model):
     phone_number = models.CharField(max_length=100)
     time_table = models.ImageField(upload_to=upload_image_to, blank=True, null=True)
     create_date = models.DateTimeField(auto_now_add=True, null=True)
-
+    create_date.editable=True
     class Meta:
         verbose_name = '신청서'
     
+    def __str__(self):
+        now = str(self.create_date)
+        return self.client.username+'신청서'+" "+now[:10]
