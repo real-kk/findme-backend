5b7c23e85955454ac17e22c5d7e32fff5b04ebf9
Kim Tae Yun <rlaxodbs2015@gmail.com>
2020-10-29 22:48:24 +0900

    Setting: more detail Hierarchy video folder s3

--- findme/task/models.py
+++ findme/task/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 from django.conf import settings
+def upload_image_to(instance, filename):
+    import os
+    filename_base, filename_ext = os.path.splitext(filename)
+    return 'video/timetable/%s%s' % (filename_base,filename_ext )
 
 class Task(models.Model):
     client = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="+", on_delete=models.CASCADE, null=True)
     create_date = models.DateTimeField(auto_now_add=True, null=True)
     title= models.CharField(max_length=200)
-    video= models.FileField(upload_to="video/", blank=True ,null=True)
+    video= models.FileField(upload_to=upload_image_to, blank=True ,null=True)
     class Meta:
         verbose_name = '영상과제'
 
     def __str__(self):
-        return self.title
+        now = str(self.create_date)
+        return self.client.username+'비디오'+" "+now[:10]
