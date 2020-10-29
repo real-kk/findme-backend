c9185e90231f80914646700b281bdc750116fa83
Kim Tae Yun <rlaxodbs2015@gmail.com>
2020-10-29 19:50:13 +0900

    Fix :  fix that User 's  'introduce'  attribute Error

--- findme/users/managers.py
+++ findme/users/managers.py
@@ -14,7 +14,7 @@ class CustomUserManager(BaseUserManager):
         if not email:
             raise ValueError(_('The Email must be set'))
         email = self.normalize_email(email)
-        user = self.model(email=email, user_type=user_type, introduct=introduce, **extra_fields)
+        user = self.model(email=email, user_type=user_type, introduce=introduce, **extra_fields)
         user.set_password(password)
         user.save()
         return user
