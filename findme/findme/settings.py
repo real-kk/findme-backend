4f8febb0a04cc48fc07f58148ef77dd01d5fe652
nuatmochoi <tjddn8770@naver.com>
2020-11-04 08:32:39 +0000

    Modify: database change from sqlite to mysql

--- findme/findme/settings.py
+++ findme/findme/settings.py
@@ -15,6 +15,7 @@ import json
 
 from findme.keys import email
 from findme.keys import s3
+from findme.keys import db
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -139,13 +140,7 @@ WSGI_APPLICATION = 'findme.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+DATABASES = db.DATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
