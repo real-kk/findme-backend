e8f325f742c2517050613e8efefcce13c02f2188
Kim Tae Yun <rlaxodbs2015@gmail.com>
2020-11-17 19:23:39 +0900

    Add :  testing code  - LineGraph (in diary)

--- findme/diary/tests.py
+++ findme/diary/tests.py
@@ -53,7 +53,6 @@ class DiaryWholeContentModelTest(TestCase):
             username='강낭콩'                                                    
         )
 
-        # Set up non-modified objects used by all test methods
         DiaryWholeContent.objects.create(whole_content='전체 내용',client=self.user)
     def test_client_is_foreignkey(self):
         diary_whole_content=DiaryWholeContent.objects.get(id=1)
@@ -106,77 +105,85 @@ class LineGraphModelTest(TestCase):
 
 
 
-
-# test용 데이터
 class DiaryWordCloudTest(TestCase):
-
-    def setUp(self):
-        self.diary = Diary()
+    @classmethod
+    def setUpTestData(self):
         self.user = User.objects.create(                                   
             email='super@gmail.com',                                                                   
             password='test',
             username='강낭콩'                                                    
         )
+
+        Diary.objects.create(content='콘텐트', title='제목',client=self.user)
+
+    def setUp(self):
         token, created = Token.objects.get_or_create(user=self.user)                
         self.client = Client(HTTP_AUTHORIZATION='Token ' + token.key)
 
-        
-    #순서제공
-    def test_review_upload_AND_update_AND_delete(self):
-        self.Test_text_extract_wordcloud_post
-        # self.Test_text_extract_wordcloud_get
-        # self.Test_text_extract_wordcloud_delete
-
     # 다이어리 업로드 + 워드클라우드 생성 test
-    def Test_text_extract_wordcloud_post(self):
+    def test_A_text_extract_wordcloud_post(self):
         url = '/diaries/'          
         dairy_data = { 
             'title':'오늘의 할일',
             'content':'테스트 드리븐 개발은 나를 성장시켜주고 새로운 것을 배우게 도와주는 즐거운 일이다. 어렵고 복잡하지만, 잘 이겨내서 좋은 개발자가 될것이다.'
         }
-        response= self.client.post('url',data=dairy_data)
-        print(response.data.content,dairy_data.content+"1")
-        self.assertEquals(response.data.content,dairy_data.content+"1")
-
+        response= self.client.post(url,data=dairy_data)
         self.assertEquals(response.status_code,201)
-        self.assertEquals(response.data.title,dairy_data.title)
-    # 다이어리 워드클라우드 가져오기 test
-    def Test_text_extract_wordcloud_get(self):
-        response= self.client.get('/reviews/'+kwargs+'/',content_type='application/json')
-        self.assertEqual(response.status_code,201)
+        self.assertEquals(response.data,dairy_data)
 
-    # 다이어리 삭제 test
-    def Test_text_extract_wordcloud_delete(self):
-        response= self.client.delete('/reviews/'+kwargs+'/',content_type='application/json')
+    # 다이어리 워드클라우드 가져오기 test
+    def test_B_text_extract_wordcloud_get_by_user(self):
+        url='/diaries/'
+        response= self.client.get(url,content_type='application/json')
         self.assertEqual(response.status_code,200)
 
-    def tearDown(self):
-        # Clean up after each test
-        self.user.delete()
-        # self.review.delete()
-class LineGraphTest(TestCase):
-
-
-    # 다이어리 업로드 + 워드클라우드 생성 test
-    def Test_text_extract_linegraph_post(self):
+    # 다이어리 삭제 test
+    def test_C_text_extract_wordcloud_delete_by_id(self):
+        url='/diaries/'
+        kwargs="2"
+        response= self.client.delete(url+kwargs+'/',content_type='application/json')
+        self.assertEqual(response.status_code,200)
         
-        response= self.client.post('/',data=review_info)
-        self.assertEqual(response.status_code,201)
-    # 다이어리 워드클라우드 가져오기 test
-    def Test_text_extract_linegraph_get(self):
 
-        response= self.client.get('/reviews/'+kwargs+'/',content_type='application/json')
+
+class LineGraphTest(TestCase):
+    @classmethod
+    def setUpTestData(self):
+        self.user = User.objects.create(                                   
+            email='super@gmail.com',                                                                   
+            password='test',
+            username='강낭콩'                                                    
+        )
+
+        Diary.objects.create(content='1일차', title='제목1',client=self.user,sentiment_score=-0.1)
+        Diary.objects.create(content='2일차', title='제목2',client=self.user,sentiment_score=0.1)
+        Diary.objects.create(content='3일차', title='제목3',client=self.user,sentiment_score=-0.2)
+        Diary.objects.create(content='4일차', title='제목4',client=self.user,sentiment_score=0.6)
+        Diary.objects.create(content='5일차', title='제목5',client=self.user,sentiment_score=0.2)
+        Diary.objects.create(content='6일차', title='제목6',client=self.user,sentiment_score=0.9)
+        Diary.objects.create(content='7일차', title='제목7',client=self.user,sentiment_score=0.8)
+
+    def setUp(self):
+        token, created = Token.objects.get_or_create(user=self.user)                
+        self.client = Client(HTTP_AUTHORIZATION='Token ' + token.key)
+    def test_text_extract_linegraph_post(self):
+        response= self.client.post('/linegraph/')
         self.assertEqual(response.status_code,201)
+        self.assertEqual(response.data.get('line_graph')[:10],'https://fi')
+
+    def test_text_extract_linegraph_get(self):
+
+        response= self.client.get('/linegraph/')
+        self.assertEqual(response.status_code,200)
+
 
 class WholeContentTest(TestCase):
 
 
-    # 다이어리 업로드 + 워드클라우드 생성 test
     def Test_whole_content_post(self):
         
         response= self.client.post('/',data=review_info)
         self.assertEqual(response.status_code,201)
-    # 다이어리 워드클라우드 가져오기 test
     def Test_whole_content_get(self):
 
         response= self.client.get('/reviews/'+kwargs+'/',content_type='application/json')
