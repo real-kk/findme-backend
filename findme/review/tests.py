252cfa5f610e5bea66bf9e09b47fc66618c3aa8c
Kim Tae Yun <rlaxodbs2015@gmail.com>
2020-11-17 20:59:15 +0900

    Add :  testing code - get Review by client

--- findme/review/tests.py
+++ findme/review/tests.py
@@ -2,7 +2,6 @@ import sys
 sys.path.append("..")
 import json 
 from django.db.models.fields.related import ForeignKey
-
 from django.test import TestCase,Client
 from .models import Review
 from users.models import User
@@ -24,29 +23,32 @@ class ReviewModelTest(TestCase):
             client=self.user,
             counselor=self.user,
             content="너무좋았어요"
+            
         )
+        self.review_id= Review.objects.values().first()['id']
+
     def test_client_is_foreignkey(self):
-        review=Review.objects.get(id=1)
+        review=Review.objects.get(id=self.review_id)
         client = review._meta.get_field('client')
         self.assertEquals(type(client),ForeignKey)
     def test_counselor_is_foreignkey(self):
-        review=Review.objects.get(id=1)
+        review=Review.objects.get(id=self.review_id)
         counselor = review._meta.get_field('counselor')
         self.assertEquals(type(counselor),ForeignKey)
 
    
     def test_content_max_length(self):
-        review = Review.objects.get(id=1)
+        review = Review.objects.get(id=self.review_id)
         max_length = review._meta.get_field('content').max_length
         self.assertEquals(max_length, 1000)
 
     def test_review_meta_verbose_name(self):
         review = Review.objects.all().first()
         self.assertEquals('후기', review._meta.verbose_name)
-
+        
 class ReviewTest(TestCase):
-
-    def setUp(self):
+    @classmethod
+    def setUpTestData(self):
         self.review = Review()
         self.user = User.objects.create(                                   
             email='super@gmail.com',                                                                   
@@ -67,6 +69,7 @@ class ReviewTest(TestCase):
         )
         self.review_id= Review.objects.values().first()['id']
 
+    def setUp(self):
         token, created = Token.objects.get_or_create(user=self.user)                
         self.client = Client(HTTP_AUTHORIZATION='Token ' + token.key)
 
@@ -99,7 +102,37 @@ class ReviewTest(TestCase):
         response= self.client.delete('/reviews/'+kwargs+'/',content_type='application/json')
         self.assertEqual(response.status_code,200)
 
-    def tearDown(self):
-        # Clean up after each test
-        self.user.delete()
-        self.review.delete()
+
+
+class GETReviewTest(TestCase):
+    @classmethod
+    def setUpTestData(self):
+        self.review = Review()
+        self.user = User.objects.create(                                   
+            email='super@gmail.com',                                                                   
+            password='test',
+            username='강낭콩'                                                    
+        )
+   
+        client1 =self.user
+        self.review=Review.objects.create(
+            client=client1,
+            counselor=client1,
+            content="너무좋았어요"
+        )
+        self.review=Review.objects.create(
+            client=client1,
+            counselor=client1,
+            content="너무좋았어요"
+        )
+        self.review_id= Review.objects.values().first()['id']
+
+    def setUp(self):
+        token, created = Token.objects.get_or_create(user=self.user)                
+        self.client = Client(HTTP_AUTHORIZATION='Token ' + token.key)
+
+    def test_get_review_by_client(self):
+        url="/reviews/counselors/"
+        kwargs=Review.objects.values().first()['client_id']
+        response= self.client.get(url+str(kwargs)+'/',content_type='application/json')
+        self.assertEqual(response.status_code,200)
