8b82c65e6efa8fe9a148a23a6bf5dae1b50164dd
Kim Tae Yun <rlaxodbs2015@gmail.com>
2020-11-17 20:19:40 +0900

    Fix :  max length  of   Review content

--- findme/review/models.py
+++ findme/review/models.py
@@ -6,7 +6,7 @@ class Review(models.Model):
     client = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="+", on_delete=models.CASCADE, null=False)
     counselor = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="+", on_delete=models.CASCADE, null=True)
     create_date = models.DateTimeField(auto_now_add=True, null=True)
-    content = models.CharField(max_length=100, null=True)
+    content = models.CharField(max_length=1000, null=True)
 
   
     class Meta:
