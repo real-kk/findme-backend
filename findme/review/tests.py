b5ae5f6c00a712b00a6bbf32d378e2332c5bf9f1
Kim Tae Yun <rlaxodbs2015@gmail.com>
2020-11-17 18:44:56 +0900

    Fix : testing code - review

--- findme/review/tests.py
+++ findme/review/tests.py
@@ -28,18 +28,14 @@ class ReviewTest(TestCase):
             counselor=client1,
             content="너무좋았어요"
         )
+        self.review_id= Review.objects.values().first()['id']
+
         token, created = Token.objects.get_or_create(user=self.user)                
         self.client = Client(HTTP_AUTHORIZATION='Token ' + token.key)
 
-        
-    #순서제공
-    def test_review_upload_AND_update_AND_delete(self):
-        self.Test_review_upload
-        self.Test_review_update
-        self.Test_review_delete
 
     # 리뷰 업로드 test
-    def Test_review_upload(self):
+    def test_A_review_upload(self):
         
         review_info ={
             "counselor":"super@gmail.com",
@@ -49,20 +45,20 @@ class ReviewTest(TestCase):
         response= self.client.post('/reviews/',data=review_info)
         self.assertEqual(response.status_code,201)
     # 리뷰 수정 test
-    def Test_review_update(self):
+    def test_B_review_update(self):
         review_info={
-            "review_id":1,
+            "review_id":self.review_id,
             "content" :" 수정됨 " 
         }
         
         response= self.client.put('/reviews/',data=json.dumps(review_info),content_type='application/json')
-        print(response.data)
         self.assertEqual(response.status_code,201)
         self.assertEqual(response.data ,'Review updated success')
 
     #리뷰 삭제 test
-    def Test_review_delete(self):
-        kwargs="2" #id
+    def test_C_review_delete(self):
+        
+        kwargs=str(self.review_id)
         response= self.client.delete('/reviews/'+kwargs+'/',content_type='application/json')
         self.assertEqual(response.status_code,200)
 
