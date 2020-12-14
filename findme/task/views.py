bbe0d69224da22c17c1c14a97e16e969b6147680
Kim Tae Yun <rlaxodbs2015@gmail.com>
2020-12-14 14:54:13 +0900

    Add - Comment for API DOCS - task

--- findme/task/views.py
+++ findme/task/views.py
@@ -44,10 +44,21 @@ def create_sentiment_graph_result(sentiments):
     return graph_image
 
 class TaskUpload(APIView):
-        
+
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
     def post(self, request, **kwargs):
+        """
+        내담자가 질문에 비디오 등록
+
+        ---
+        # /tasks/<id:int>/
+        ## headers
+            - Authorization : Token "key 값" [ex> Token 822a24a314dfbc387128d82af6b952191dd71651]
+        ## body parameter
+            - video : 내담자 이메일 [ex> abc@amail.com]
+            - question : 등록할 질문 내용 [ex> 가장 좋아하는 음식은?]
+        """        
         if kwargs.get('id') is None:
             return Response('invalid request', status=status.HTTP_400_BAD_REQUEST)
         serializers = TaskSerializer(data=request.data)
@@ -69,6 +80,15 @@ class TaskDetail(APIView):
     permission_classes = [IsAuthenticated]
 
     def delete(self,request,**kwargs):
+        """
+        과제 삭제
+
+        ---
+        # /tasks/
+        ## headers
+            - Authorization : Token "key 값" [ex> Token 822a24a314dfbc387128d82af6b952191dd71651]
+        """
+
         if request.user.user_type != '1':
             return Response("Only Counselor can delete Task", status=status.HTTP_403_FORBIDDEN)
 
@@ -89,6 +109,18 @@ class AddTaskQuestion(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
+        """
+        내담자에게 질문 등록
+
+        ---
+        # /tasks/questions/
+        ## headers
+            - Authorization : Token "key 값" [ex> Token 822a24a314dfbc387128d82af6b952191dd71651]
+        ## body parameter
+            - client : 내담자 이메일 [ex> abc@amail.com]
+            - question : 등록할 질문 내용 [ex> 가장 좋아하는 음식은?]
+        """
+
         serializer = TaskQuestionSerializer(data=request.data)
         if serializer.is_valid():
             client_email = request.data.get('client')
@@ -99,6 +131,15 @@ class AddTaskQuestion(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request):
+        """
+        내담자의 질문 조회
+
+        ---
+        # /tasks/questions/
+        ## headers
+            - Authorization : Token "key 값" [ex> Token 822a24a314dfbc387128d82af6b952191dd71651]
+        """
+
         questions = Task.objects.filter(client=request.user)
         serializer = TaskQuestionSerializer(questions, many=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
@@ -108,6 +149,19 @@ class TaskQuestionForCounselor(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
+        """
+        상담사의 질문 조회
+
+        ---
+        # /tasks/questions_counselor/
+        ## headers
+            - Authorization : Token "key 값" [ex> Token 822a24a314dfbc387128d82af6b952191dd71651]
+        ## body parameter
+            - client : 내담자 이메일 [ex> abc@amail.com]
+
+        """
+
+
         client_email = request.GET.get('client')
         client = User.objects.get(email=client_email)
         questions = Task.objects.filter(client=client, counselor=request.user)
@@ -119,6 +173,16 @@ class VideoProcessing(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, **kwargs):
+        """
+        비디오 감정분석 진행
+
+        ---
+        # /tasks/process_videos/<id:int>/
+        ## headers
+            - Authorization : Token "key 값" [ex> Token 822a24a314dfbc387128d82af6b952191dd71651]
+
+        """
+
         if kwargs.get('id') is None:
             return Response('invalid request', status=status.HTTP_400_BAD_REQUEST)
         new_video = Task.objects.get(pk=kwargs.get("id"))
@@ -134,9 +198,21 @@ class VideoProcessing(APIView):
 
 class GetSentiment(APIView):
     def post(self, request):
+        """
+        분석된 감정 조회
+
+        ---
+        # /tasks/sentiment/
+        ## headers
+            - Authorization : Token "key 값" [ex> Token 822a24a314dfbc387128d82af6b952191dd71651]
+        ## body parameter
+            - sentiments : 내담자 이메일 [ex> abc@amail.com]
+            - key : 파일 이름
+        """
+
+
         sentiments = request.data.get("sentiments")
         filename = request.data.get("key")
-        print(filename)
         task = Task.objects.filter(video=filename)[0]
         sentiment_graph = create_sentiment_graph_result(sentiments)
         task.graph.save("sentiment_graph" + datetime.now().strftime('%Y-%m-%d_%H%M%S') + ".png", sentiment_graph)
@@ -149,11 +225,34 @@ class MakeSentimentLinegraph(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
+        """
+        감정 라인 그래프 생성
+
+        ---
+        # /tasks/sentiment_graphs/
+        ## headers
+            - Authorization : Token "key 값" [ex> Token 822a24a314dfbc387128d82af6b952191dd71651]
+        ## body parameter
+            - id : task의 아이디
+        """
+
+
         task = Task.objects.get(id=request.data.get('id'))
         serializer = SentimentGraphSerializer(task)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
         
     def get(self, request):
+        """
+        감정 라인 그래프 조회
+
+        ---
+        # /tasks/sentiment_graphs/
+        ## headers
+            - Authorization : Token "key 값" [ex> Token 822a24a314dfbc387128d82af6b952191dd71651]
+        ## body parameter
+            - id : task의 아이디
+        """
+
         pk = request.GET.get('id')
         task = Task.objects.get(id=pk)
         serializer = SentimentGraphSerializer(task)
